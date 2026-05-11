58f55a8b19a669d9979855d164aee45ba5e865d6
Carlos Tadeo <carlosdantetadeo@gmail.com>
2026-05-11 00:03:17 -0500

    fix: restaurar credenciales supabase

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-
+const SUPABASE_URL = 'https://dfyhpobirbezfqnhutcm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmeWhwb2JpcmJlemZxbmh1dGNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzODk3MzAsImV4cCI6MjA5Mzk2NTczMH0.jJkavVu7rcezx7QguiF1p4ukHAorv0_adTaTnhkn-9c';
